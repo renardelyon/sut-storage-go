f31416821ace962cad388117c88f94aea840b767
renardelyon <renard.elyon.r@gmail.com>
2022-07-11 00:04:30 +0700

    modified add file handler

--- domain/storage/service/add_file.go
+++ domain/storage/service/add_file.go
@@ -3,13 +3,13 @@ package service
 import (
 	"fmt"
 	"log"
+	"sut-storage-go/domain/storage/model"
 	"sut-storage-go/lib/helper"
 	storagepb "sut-storage-go/pb/storage"
 
 	"sut-storage-go/lib/utils"
 
-	"google.golang.org/api/drive/v3"
-	"google.golang.org/api/googleapi"
+	"google.golang.org/api/drive/v2"
 )
 
 func (s *Service) AddFile(stream storagepb.StorageService_AddFileServer) error {
@@ -52,14 +52,45 @@ func (s *Service) AddFile(stream storagepb.StorageService_AddFileServer) error {
 	defer file.Close()
 	defer tmpfCreator.DeleteTempFile()
 
-	f := &drive.File{Name: fmt.Sprintf("%s.xlsx", userId), Parents: []string{s.conf.FolderId}}
+	f := &drive.File{
+		Title:    fmt.Sprintf("%s.xlsx", userId),
+		MimeType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+	}
+
+	p := &drive.ParentReference{Id: s.conf.FolderId}
+	f.Parents = []*drive.ParentReference{p}
 
 	res, err := s.driveSrv.Files.
-		Create(f).
-		Media(file, googleapi.ContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")).
+		Insert(f).
+		Media(file).
 		Do()
 	if err != nil {
 		log.Fatalln("error when upload file: ", err)
+	}
+
+	var fileInfo model.File
+
+	if result := s.H.DB.Where(&model.File{UserId: userId}).First(&fileInfo); result.Error != nil {
+		result := s.H.DB.Create(&model.File{
+			Id:     res.Id,
+			UserId: userId,
+		})
+		if result.Error != nil {
+			log.Println(result.Error)
+			return result.Error
+		}
+	} else {
+		err = s.DeleteFile(ctx, fileInfo.Id)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		result := s.H.DB.Model(&model.File{}).Where(&model.File{UserId: userId}).Update("id", res.Id)
+		if result.Error != nil {
+			log.Println(result.Error)
+			return result.Error
+		}
 	}
 
 	err = stream.SendAndClose(&storagepb.UploadResponse{
